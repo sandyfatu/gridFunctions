c1227b67440584e2d92519ee5b7d8a817541c6cd
Sandra Fatu Ogblechi <scarletprissy@gmail.com>
2025-03-04 08:22:45 -0800

    i added a clock function

---
+++ script.js
@@ -0,0 +1,7 @@
+const displayTime = () => {
+  const clock = document.querySelector("#clock");
+  const now = new Date();
+  clock.textContent = now.toLocaleTimeString();
+};
+displayTime();
+setInterval(displayTime, 1000);
